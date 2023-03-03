5f2f6dd02ffa77be3f60279e4b7aa950b628ec95
Dani Ponce Mendoza <danniscreamer@gmail.com>
2023-03-02 18:24:41 -0600

    se completó el formulario

--- firebase-proyect/src/components/Reserva.jsx
+++ firebase-proyect/src/components/Reserva.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { db } from "../Firebase/firebase";
-import { collection, getDocs, addDoc, doc } from "firebase/firestore";
+import { collection, getDocs, addDoc, doc, deleteDoc } from "firebase/firestore";
 
 function Reserva(){
+    
     // console.log(import.meta.env.VITE_BACKEND_HOLA)
     // console.log(import.meta.env.DB_PASSWORD)
      const [users,setuser]= useState([])
      const useCollectionRef= collection(db,'reservaciones')
       const [name, setName]= useState("")
-     const [correo, setCorreo]= useState("")
+      const [correo, setCorreo]= useState("")
       const [mesa, setMesa]= useState(0)
    
      const createUser = async() => {
@@ -21,6 +22,12 @@ function Reserva(){
        const data = await getDocs(useCollectionRef)
        console.log(data)
       setuser(data.docs.map((doc) =>({...doc.data(), id: doc.id})))
+     }
+
+     const borrarUsuario = async(id) =>{
+        const userDoc = doc(db,'reservaciones',id)
+        await deleteDoc(userDoc)
+        getUsers();
      }
 
      useEffect(()=>{
@@ -45,6 +52,7 @@ function Reserva(){
                     <h1>Nombre: {item.nombre}</h1>
                     <h1>Correo: {item.correo}</h1>
                     <h1>Mesa: {item.mesa}</h1>
+                    <button onClick={() => borrarUsuario(item.id)}>X</button>
                 </div>
             )
         })} 
